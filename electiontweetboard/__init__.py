a402359ad05aee7b5bee332cb3f57460535777e4
Shantanu Phadke <shantanu.phadke@kellogg.northwestern.edu>
2024-01-15 11:24:09 -0600

    Trying to fix the inconsistent tabs/spaces issue

--- electiontweetboard/__init__.py
+++ electiontweetboard/__init__.py
@@ -91,9 +91,9 @@ def masterUpdateMethod():
     # object made above.
     with app.app_context():
         commands.loadAllSentimentDistributions(all_politician_sentiment_data)
+		masterGeographicSentimentAnalyzer()	
     # The rest of the stuff the Frontend / UI should take care of.
-    # tokenizer_minify('cardiffnlp/twitter-roberta-base-sentiment', 15000, 'twitter-roberta-minified-15k')
-	masterGeographicSentimentAnalyzer()		
+    # tokenizer_minify('cardiffnlp/twitter-roberta-base-sentiment', 15000, 'twitter-roberta-minified-15k')	
 
 db_update_scheduler = BackgroundScheduler()
 # It'll be run once right away when the script is first started
