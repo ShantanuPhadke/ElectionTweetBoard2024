8a8defab7f70da8fc6daff5425228bc851b4c1d6
Shantanu Phadke <shantanu.phadke@kellogg.northwestern.edu>
2024-01-16 17:44:02 -0600

    Fixing the tokenizer issue by supplying it a value for the parameter max_length of 512

--- electiontweetboard/nlp/SentimentAnalyzer.py
+++ electiontweetboard/nlp/SentimentAnalyzer.py
@@ -25,7 +25,7 @@ class SentimentAnalyzer:
 			project_dir = os.path.dirname(__file__)
 			# SentimentAnalyzer.tokenizer = PreTrainedTokenizerFast(tokenizer_file=os.path.join(project_dir, "twitter-roberta-minified-15k/tokenizer.json"))
 			SentimentAnalyzer.tokenizer = AutoTokenizer.from_pretrained('cardiffnlp/twitter-roberta-base-sentiment')
-			SentimentAnalyzer.tokenizer.model_max_length = 514
+			SentimentAnalyzer.tokenizer.model_max_length = 512
 			SentimentAnalyzer.tokenizer.padding = 'max_length'
 			SentimentAnalyzer.tokenizer.truncation = True
 	
@@ -48,7 +48,7 @@ class SentimentAnalyzer:
 		# tokenizer = AutoTokenizer.from_pretrained(roberta)
 		labels = ['Negative', 'Neutral', 'Positive']
 		# actual sentiment analysis
-		encoded_tweet = SentimentAnalyzer.tokenizer(tweet_processed, return_tensors='pt')
+		encoded_tweet = SentimentAnalyzer.tokenizer(tweet_processed, return_tensors='pt', max_length=512)
 		# print('encoded_tweet = ' + str(encoded_tweet))
 		output = model(**encoded_tweet)
 		scores = output[0][0].detach().numpy()
