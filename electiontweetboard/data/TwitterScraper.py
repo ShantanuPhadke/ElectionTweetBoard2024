f6d01ac97538e9849217daf80f6137f8ec3bbeb7
Shantanu Phadke <shantanu.phadke@kellogg.northwestern.edu>
2024-02-11 15:58:49 -0600

    Trying to use multiprocessing apply_async to get past issue where flask functions just get stuck

--- electiontweetboard/data/TwitterScraper.py
+++ electiontweetboard/data/TwitterScraper.py
@@ -1,4 +1,5 @@
 from ntscraper import Nitter
+import multiprocessing
 
 class TwitterScraper:
 	# This will represent the one and only 'true' Twitter Connection 
@@ -31,12 +32,15 @@ class TwitterScraper:
 		while True:
 			print('IM IN THE LOOP!')
 			try:
-				search_results = TwitterScraper.scraper.get_tweets(query_string, mode='term', number=number_tweets, near=near, language='en')
-				print('search_results = ' + str(len(search_results['tweets'])))
-				if len(search_results['tweets']) > 0:
-					break
-				else:
-					continue
+				with multiprocessing.Pool(1) as pool:
+					# search_results = TwitterScraper.scraper.get_tweets(query_string, mode='term', number=number_tweets, near=near, language='en')
+					result = pool.apply_async(TwitterScraper.scraper.get_tweets, args=(query_string), kwds=('mode': 'term', 'number': number_tweets, 'near': near, 'language': 'en'))
+					results_dict = result.get(300)
+					print('search_results = ' + str(len(search_results['tweets'])))
+					if len(search_results['tweets']) > 0:
+						break
+					else:
+						continue
 			except Exception as e:
 				print('\nException encountered in TwitterScraper!! e = ' + str(e) + '\n')
 				continue
